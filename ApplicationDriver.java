e36a381c019d31cfff8751a29f0b1faee002eb38
revathiguruju17 <42278389+revathiguruju17@users.noreply.github.com>
2018-10-12 13:13:04 +0530

    Add files via upload

--- ApplicationDriver.java
+++ ApplicationDriver.java
@@ -1,76 +1,86 @@
 package bicycleRentShop;
 
 public class ApplicationDriver {
+
+    private static final int DISPLAY_MENU = 1;
+    private static final int DISPLAY_LIST_OF_BICYCLES = 2;
+    private static final int BICYCLE_HIRING = 3;
+    private static final int BICYCLE_RETURNING = 4;
+    private static final int CUSTOMER_HISTORY = 5;
+    private static final int OWNER_HISTORY = 6;
+    private static final int QUIT = 7;
+
     void startApplication(Owner owner) {
         OutputDriver outputDriver = new OutputDriver();
         outputDriver.printMenu();
         doCustomerRequest(owner);
     }
 
-    private  void doCustomerRequest(Owner owner) {
+    private void doCustomerRequest(Owner owner) {
         OutputDriver outputDriver = new OutputDriver();
         InputDriver inputDriver = new InputDriver();
         int option;
         do {
             option = inputDriver.scanUserInputAsInteger();
             switch (option) {
-                case 1: {
+                case DISPLAY_MENU: {
                     outputDriver.printMenu();
                     break;
                 }
-                case 2: {
+                case DISPLAY_LIST_OF_BICYCLES: {
                     owner.displayTheListOfBicycles();
                     break;
                 }
-                case 3: {
+                case BICYCLE_HIRING: {
                     String ownerChoice = owner.permissionFromOwnerToHireABicycle();
                     if (ownerChoice.equals("yes")) {
-                        hiringProcess(owner,inputDriver,outputDriver);
+                        hiringProcess(owner, inputDriver, outputDriver);
                     } else {
                         outputDriver.intimateUserAboutOwnerDecisionForHiringABicycle();
                     }
                     break;
                 }
-                case 4: {
-                    returningProcess(owner,inputDriver,outputDriver);
+                case BICYCLE_RETURNING: {
+                    returningProcess(owner, inputDriver, outputDriver);
                     break;
                 }
-                case 5: {
+                case CUSTOMER_HISTORY: {
                     outputDriver.enterCustomerName();
                     String customerID = inputDriver.scanUserInputAsString();
                     Customer customer = owner.getCustomer(customerID);
-                    customer.getHiredBicycles(customerID,owner);
+                    customer.getHiredBicycles(customerID, owner);
                     break;
                 }
-                case 6: {
+                case OWNER_HISTORY: {
                     owner.displayAllTheCustomersHistory(owner);
                     break;
                 }
-                case 7: {
+                case QUIT: {
                     return;
                 }
             }
-        } while (option<7 && option>0);
+        } while (option < 7 && option > 0);
     }
 
     private void returningProcess(Owner owner, InputDriver inputDriver, OutputDriver outputDriver) {
         outputDriver.customerAndBicycleDetails();
         String customerID = inputDriver.scanUserInputAsString();
         String bicycleID = inputDriver.scanUserInputAsString();
-        Customer customer =owner.getCustomer(customerID);
+        Customer customer = owner.getCustomer(customerID);
         customer.returnBicycle(customerID, bicycleID, owner);
-        askCustomerForInvoice(customerID,bicycleID,owner,inputDriver,outputDriver);
+        askCustomerForInvoice(customerID, bicycleID, owner, inputDriver, outputDriver);
     }
 
-    private void askCustomerForInvoice(String customerID, String bicycleID, Owner owner,InputDriver inputDriver,OutputDriver outputDriver) {
+    private void askCustomerForInvoice(String customerID, String bicycleID, Owner owner,
+                                       InputDriver inputDriver, OutputDriver outputDriver) {
         outputDriver.askUserAboutInvoice();
         String userChoice = inputDriver.scanUserInputAsString();
-        if(userChoice.equals("yes")) {
-            owner.generateInvoice(customerID,bicycleID);
+        if (userChoice.equals("yes")) {
+            owner.generateInvoice(customerID, bicycleID);
         }
     }
 
-    private void hiringProcess(Owner owner,InputDriver inputDriver,OutputDriver outputDriver) {
+    private void hiringProcess(Owner owner, InputDriver inputDriver, OutputDriver outputDriver) {
         outputDriver.customerAndBicycleDetails();
         String customerID = inputDriver.scanUserInputAsString();
         String bicycleID = inputDriver.scanUserInputAsString();
